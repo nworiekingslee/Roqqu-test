51a2ad9c1529c38627e239172b7ba78c3667ceac
Nworie Kingsley <obizzkingsley@gmail.com>
2022-09-16 12:36:52 +0100

    Setup project asset and variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import "./App.css";
+import "./styles/App.css";
+import Topnav from "./components/Topnav";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Roqqu</h1>
+      <Topnav />
     </div>
   );
 }
---
+++ src/components/Topnav.jsx
@@ -0,0 +1,7 @@
+import "../styles/Topnav.css";
+
+const Topnav = () => {
+  return <div className="top-nav">Topnav</div>;
+};
+
+export default Topnav;
