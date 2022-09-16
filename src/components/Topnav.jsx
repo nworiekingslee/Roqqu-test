dcb5e3e5fcbc6271865ff3a3f2040b87efba34e5
Nworie Kingsley <obizzkingsley@gmail.com>
2022-09-16 20:16:53 +0100

    Add sidenav

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -6,20 +6,21 @@ import notification from "../images/navigation/notification.svg";
 const Topnav = () => {
   return (
     <div className="top-nav">
-      <img src={logo} alt="Roqqu" className="nav-brand" />
-
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          width: 100,
-          margin: 32,
-        }}
-      >
-        <img src={notification} alt="notification" className="icon" />
-        <div className="user-profile">
-          <img src={profile} alt="user" />
+      <div className="link-group">
+        <img src={logo} alt="Roqqu" className="nav-brand" />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: 100,
+            margin: 24,
+          }}
+        >
+          <img src={notification} alt="notification" className="icon" />
+          <div className="user-profile">
+            <img src={profile} alt="user" />
+          </div>
         </div>
       </div>
     </div>
